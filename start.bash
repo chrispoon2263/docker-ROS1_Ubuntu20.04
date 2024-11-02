8fbec3213adb6a75cb17606424ba77edc132e324
ChrisP <93444123+chrispoon2263@users.noreply.github.com>
2024-11-02 11:31:39 -0700

    Update start.bash

--- start.bash
+++ start.bash
@@ -5,3 +5,4 @@ xhost +local:docker
 
 # Run Docker command
 # sudo docker start <container name> && sudo docker exec -it <container name> /bin/bash 
+sudo docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "/home/christopher/Desktop/ROB456_IntelligentRobots:/home/ROB456" ros_noetic_custom
